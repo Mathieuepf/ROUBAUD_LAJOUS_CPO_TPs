b234023cb5706352ae86fe5d1ad591b54ed66c49
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-10-04 17:15:27 +0200

    v3 (pas vérifié

--- guessMyNumber_LAJOUS_ROUBAUD/src/guessmynumber_lajous_roubaud/GuessMyNumber_LAJOUS_ROUBAUD.java
+++ guessMyNumber_LAJOUS_ROUBAUD/src/guessmynumber_lajous_roubaud/GuessMyNumber_LAJOUS_ROUBAUD.java
@@ -25,10 +25,50 @@ public class GuessMyNumber_LAJOUS_ROUBAUD {
         int nombreA = generateurAleat.nextInt(100);
         int essaie = -2;
         int compteur = 0;
+        int niveau = 0;
         
         //istructions
-        while (nombreA != essaie){
-            System.out.println("Entrez un nombre aléatoire entre 0 et 100");
+        while (niveau != 1 && niveau != 2 && niveau != 3){
+            System.out.println("Choisissez le niveau de difficulté\nniveau 1 : intervale entre 0 et 50 et indications simplifiées\nniveau 2 : intervale entre 0 et 100 et indications standards\nniveau 3 : intervale entre 0 et 100 nombre d'essaies limités à 10 et indications standards");
+            niveau = sc.nextInt();
+            if (niveau == 1 || niveau == 2 || niveau == 3){
+                break;
+            }
+        }
+            
+        if (niveau == 1){
+            nombreA = generateurAleat.nextInt(50);
+            while (nombreA != essaie){
+                System.out.println("Entrez votre essaie entre 0 et 50");
+                essaie = sc.nextInt();
+                if(essaie < nombreA){
+                    if (essaie+20 < nombreA){
+                        System.out.println("vraiment trop petit");
+                    }
+                    else{
+                        System.out.println("trop petit");
+                    }    
+                }
+                else{
+                    if(essaie > nombreA){
+                        if (essaie-20 > nombreA){
+                            System.out.println("vraiment trop grand");
+                        }
+                        else{
+                            System.out.println("trop grand");
+                        }
+                    }
+                }
+                compteur ++;
+            }
+            System.out.println("gagné avec " + compteur + " tentatives");
+        }
+        
+        
+        
+        if (niveau == 2){
+            while (nombreA != essaie){
+            System.out.println("Entrez votre essaie entre 0 et 100");
             essaie = sc.nextInt();
             if(essaie < nombreA){
                 System.out.println("trop petit"); 
@@ -37,8 +77,29 @@ public class GuessMyNumber_LAJOUS_ROUBAUD {
                 System.out.println("trop grand");
             }
             compteur ++;
+            }
+            System.out.println("gagné avec " + compteur + " tentatives");
         }
-        System.out.println("gagné avec " + compteur + " tentatives");
+        
+        
+        if (niveau == 3){
+            while (nombreA != essaie || compteur >= 10){
+                System.out.println("Entrez votre essaie entre 0 et 100");
+                essaie = sc.nextInt();
+                if(essaie < nombreA){
+                    System.out.println("trop petit"); 
+                }
+                if(essaie > nombreA) {
+                    System.out.println("trop grand");
+                }
+                compteur ++;
+            }
+            if (compteur >= 10){
+                System.out.println("perdu nombre d'essaies supérieur à 10");
+            }
+            else{
+                System.out.println("gagné avec " + compteur + " tentatives");
+            }
+        }
     }
-    
 }
