848d3664606fe84decea6c3605091a8a5c0111e0
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-10-04 15:14:14 +0200

    ex1 tp

--- manipNombesInt_LAJOUS_ROUBAUD/src/manipnombesint_lajous_roubaud/ManipNombesInt_LAJOUS_ROUBAUD.java
+++ manipNombesInt_LAJOUS_ROUBAUD/src/manipnombesint_lajous_roubaud/ManipNombesInt_LAJOUS_ROUBAUD.java
@@ -14,7 +14,7 @@ public class ManipNombesInt_LAJOUS_ROUBAUD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // yes les tests
     }
     
 }
