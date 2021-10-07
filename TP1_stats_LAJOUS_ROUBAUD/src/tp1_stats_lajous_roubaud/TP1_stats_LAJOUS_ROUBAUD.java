327d38b5a94a2b3c43fbf204b572e4b411fd25d6
malol <malol@192.168.115.2>
2021-10-07 11:04:56 +0200

    TP1_stats terminé et validé

--- TP1_stats_LAJOUS_ROUBAUD/src/tp1_stats_lajous_roubaud/TP1_stats_LAJOUS_ROUBAUD.java
+++ TP1_stats_LAJOUS_ROUBAUD/src/tp1_stats_lajous_roubaud/TP1_stats_LAJOUS_ROUBAUD.java
@@ -1,8 +1,10 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+ 
+*/
 package tp1_stats_lajous_roubaud;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -14,7 +16,28 @@ public class TP1_stats_LAJOUS_ROUBAUD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //  Modules
+        Scanner sc;
+        sc = new Scanner (System.in);
+        Random ta=new Random();
+        
+        //  Intialisation variables
+        float tab []=new float[6];
+        int m=0;
+        int face=ta.nextInt(6);
+        
+        //  Instruction
+        System.out.println("Veuillez saisir une nombre entier:...");
+        m=sc.nextInt();
+        
+        for (int i=0; i<m; i++){
+            face=ta.nextInt(6);
+            tab[face]=tab[face]+1;
+        }
+        for(int a=0; a<6;a++){
+            System.out.println("La face "+(a+1)+" est tombée "+tab[a]+" fois.\n"
+                    + "Sois un rapport de "+tab[a]/m+" % sur "+m+" lancer.\n");
+        }
     }
     
 }
