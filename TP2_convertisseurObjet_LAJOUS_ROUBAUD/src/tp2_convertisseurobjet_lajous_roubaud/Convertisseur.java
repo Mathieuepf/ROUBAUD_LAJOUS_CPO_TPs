02944864c4eea3182914845f7015824d1e2647e8
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-10-09 13:35:16 +0200

    début de l'exercice

---
+++ TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/Convertisseur.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_convertisseurobjet_lajous_roubaud;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class Convertisseur {
+    int nbConversions;
+    
+    public void Convertisseur(){
+        
+    }
+    
+    public double CelsiusVersKelvin(double entree){
+        return entree;
+    }
+    
+    public double KelvinVersCelsius(double kelvin){
+        return kelvin;
+    }
+    
+    public double FarenheitVersCelsius(double Farenheit){
+        return Farenheit;
+    }
+    
+    public double CelsuisVersFarenheit(double Celsuis){
+        return Celsuis;
+    }
+    
+    public double FarenheitVersKelvin(double Far2){
+        return Far2;
+    }
+    
+    public double KelvinVersFarenheit(double Kelvin2){
+        return Kelvin2;
+    }
+    
+    @Override 
+    public String toString() {
+        return "lol";
+    }
+}
---
+++ TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/TP2_convertisseurObjet_LAJOUS_ROUBAUD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp2_convertisseurobjet_lajous_roubaud;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class TP2_convertisseurObjet_LAJOUS_ROUBAUD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
