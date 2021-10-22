baf7d48d88a6abe670951fb58790342dc3dc79e4
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-10-22 14:43:49 +0200

    tp terminé

--- TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/Convertisseur.java
+++ TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/Convertisseur.java
@@ -12,47 +12,53 @@ public class Convertisseur {
     int nbConversions;
     
     public Convertisseur () { 
-        nbConversions = 0 ;
+        nbConversions=0 ;
     }
     
     public double CelsiusVersKelvin(double tempCelcius){
         double tempKelvin;
         tempKelvin=tempCelcius+273.15;
+        nbConversions += 1;
         return tempKelvin;
     }
     
     public double KelvinVersCelsius(double tempKelvin){
         double tempCelcius;
         tempCelcius=tempKelvin-273.15;
+        nbConversions += 1;
         return tempCelcius;
     }
     
     public double FarenheitVersCelsius(double tempFarenheit){
         double tempCelcius;
         tempCelcius=(tempFarenheit-32)*5.0/9.0;
+        nbConversions += 1;
         return tempCelcius;
     }
     
     public double CelsuisVersFarenheit(double tempCelcius){
         double tempFarenheit;
         tempFarenheit=tempCelcius*9.0/5.0+32;
+        nbConversions += 1;
         return tempFarenheit;
     }
     
     public double FarenheitVersKelvin(double tempFarenheit){
         double tempKelvin;
         tempKelvin=(tempFarenheit+459.67)*5.0/9.0;
+        nbConversions += 1;
         return tempKelvin;
     }
     
     public double KelvinVersFarenheit(double tempKelvin){
         double tempFarenheit;
-        tempFarenheit=tempKelvin*9/5-459.67;
+        tempFarenheit=tempKelvin*9.0/5.0-459.67;
+        nbConversions += 1;
         return tempFarenheit;
     }
     
     @Override 
     public String toString() {
-        return "nb de conversions"+ nbConversions;
+        return " nb de conversions "+ nbConversions;
     }
 }
--- TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/TP2_convertisseurObjet_LAJOUS_ROUBAUD.java
+++ TP2_convertisseurObjet_LAJOUS_ROUBAUD/src/tp2_convertisseurobjet_lajous_roubaud/TP2_convertisseurObjet_LAJOUS_ROUBAUD.java
@@ -15,47 +15,57 @@ public class TP2_convertisseurObjet_LAJOUS_ROUBAUD {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
         //  Téléchargement Scanner
         Scanner sc;
         sc = new Scanner(System.in);
+        
+        
+        //  Déclaration variables:
+        double reel;
+        int convertion;
         Convertisseur Temperature;
         Temperature = new Convertisseur();
         
-        //  Déclaration variables:
-        double reel=0;
-        int convertion=0;
         
+        while(1==1){
         //  Commandes globales
         System.out.println("Bonjour, saisissez une valeur :");
         reel=sc.nextDouble();
-        System.out.println("Saisissez la conversion que vous souhaiter effectuer :"
-                + "1) De Celcius vers Kelvin"
-                + "2) De Kelvin vers Celcius"
-                + "3) De Farenheit vers Celcius"
-                + "4) De Celcius vers Farenheit"
-                + "5) De Kelvin vers Farenheit"
-                + "6) De Farenheit vers Kelvin");
+        System.out.println("""
+                           Saisissez la conversion que vous souhaiter effectuer :
+                           1) De Celcius vers Kelvin
+                           2) De Kelvin vers Celcius
+                           3) De Farenheit vers Celcius
+                           4) De Celcius vers Farenheit
+                           5) De Kelvin vers Farenheit
+                           6) De Farenheit vers Kelvin""");
         convertion=sc.nextInt();
         
         if (convertion==1){
-            System.out.println(reel+"degré Celcius est égal à "+Temperature.CelsiusVersKelvin(reel)+"° Kelvin");
+            System.out.println(reel+" degré Celcius est égal à "+Temperature.CelsiusVersKelvin(reel)+"° Kelvin" + Temperature);
         }
         if (convertion==2){
-            System.out.println(reel+"degré Kelvin est égal à "+KelvinVersCelcius(reel)+"° Celcius");
+            System.out.println(reel+" degré Kelvin est égal à "+Temperature.KelvinVersCelsius(reel)+"° Celcius" + Temperature);
         }
         if (convertion==3){
-            System.out.println(reel+"degré Farenheit est égal à "+FarenheitVersCelcius(reel)+"° Celcius");
+            System.out.println(reel+" degré Farenheit est égal à "+Temperature.FarenheitVersCelsius(reel)+"° Celcius" + Temperature);
         }
         if (convertion==4){
-            System.out.println(reel+"degré Celcius est égal à "+CelciusVersFarenheit(reel)+"° Farenheit");
+            System.out.println(reel+" degré Celcius est égal à "+Temperature.CelsuisVersFarenheit(reel)+"° Farenheit" + Temperature);
         }
         if (convertion==5){
-            System.out.println(reel+"degré Kelvin est égal à "+KelvinVersFarenheit(reel)+"° Farenheit");
+            System.out.println(reel+" degré Kelvin est égal à "+Temperature.KelvinVersFarenheit(reel)+"° Farenheit" + Temperature);
         }
         if (convertion==6){
-            System.out.println(reel+"degré Farenheit est égal à "+FarenheitVersKelvin(reel)+"° Kelvin");
+            System.out.println(reel + " degré Farenheit est égal à " + Temperature.FarenheitVersKelvin(reel)+"° Kelvin" + Temperature);
+        }
+        if (convertion != 1 && convertion != 2 && convertion != 3 && convertion!=4 && convertion!=5 && convertion!=6){
+            break;
+        }
+        
         }
     }
-    
 }
