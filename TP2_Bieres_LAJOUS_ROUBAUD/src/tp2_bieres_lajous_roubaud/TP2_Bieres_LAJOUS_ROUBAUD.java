6c78c0db3f2f7bc2f329693f00896c70a43647a3
malol <malol@192.168.115.185>
2021-10-22 15:27:17 +0200

    Dépot partie 1 TP2 V.Malô

--- TP2_Bieres_LAJOUS_ROUBAUD/src/tp2_bieres_lajous_roubaud/TP2_Bieres_LAJOUS_ROUBAUD.java
+++ TP2_Bieres_LAJOUS_ROUBAUD/src/tp2_bieres_lajous_roubaud/TP2_Bieres_LAJOUS_ROUBAUD.java
@@ -1,7 +1,8 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+    TP2 Ex 1 : Bieres
+    LAJOUS Malô & ROUBAUD Mathieu
+    Octobre 2021
+*/
 package tp2_bieres_lajous_roubaud;
 
 /**
@@ -14,7 +15,30 @@ public class TP2_Bieres_LAJOUS_ROUBAUD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        BouteilleBiere uneBiere =new BouteilleBiere("Cuvée des trolls",7.0,"Dubuisson");
+        uneBiere.lireEtiquette();
+        
+        BouteilleBiere secondeBiere = new BouteilleBiere("Leffe", 6.6 , "Abbaye de Leffe");
+        secondeBiere.lireEtiquette();        
+
+        BouteilleBiere thirdbeer = new BouteilleBiere ("Heineken", 5.0 , "Brasseries Heineken");
+        thirdbeer.lireEtiquette();
+        
+        BouteilleBiere fourthbeer = new BouteilleBiere ("1664", 5.5 , "Brasserie d'Obernai");
+        fourthbeer.lireEtiquette();
+        
+        BouteilleBiere fifthbeer = new BouteilleBiere ("La Goudale", 7.2, "Brasserie Goudale");
+        fifthbeer.lireEtiquette();
+        
+        System.out.println(fifthbeer.ouverte);
+        fifthbeer.Decapsuler();
+        System.out.println(fifthbeer.ouverte);
+        // S'affiche false puis true donc l'attibut ouverte change bien de valeur grâce à la méthode Decapsuler
+        
+        System.out.println(uneBiere) ;
+        uneBiere.Decapsuler();
+        System.out.println(uneBiere);
+
     }
     
 }
