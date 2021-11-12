df2060eeb6c4e9f47681b0d22ef7b2cdd9a622d0
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-11-12 17:41:56 +0100

    début de la class Partie (V1)

---
+++ SP4_console_LAJOUS _ROUBAUD/src/sp4_console_lajous/_roubaud/Partie.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_lajous._roubaud;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class Partie {
+    Joueur [] ListeJouer = new Joueur[2];
+    Joueur joueurCourant;
+    Grille grilleJeu
+}
