2a732bab0b124a67df2ee976cf0d6303f29b55a7
malol <malol@192.168.115.185>
2021-11-12 18:16:10 +0100

    SP4 / Console V1 : Classe Grille --> Arguments terminés, méthode Grille() OK

--- SP4_console_LAJOUS _ROUBAUD/src/sp4_console_lajous/_roubaud/Grille.java
+++ SP4_console_LAJOUS _ROUBAUD/src/sp4_console_lajous/_roubaud/Grille.java
@@ -10,5 +10,13 @@ package sp4_console_lajous._roubaud;
  * @author malol
  */
 public class Grille {
+    Cellule CellulesJeu[][] = new Cellule [6][7];
     
+    public void Grille (){
+        for (int l = 0;l<6;l++){
+            for(int c=0;c<7;c++){
+                CellulesJeu[l][c]= new Cellule();
+            }
+        }
+    }
 }
