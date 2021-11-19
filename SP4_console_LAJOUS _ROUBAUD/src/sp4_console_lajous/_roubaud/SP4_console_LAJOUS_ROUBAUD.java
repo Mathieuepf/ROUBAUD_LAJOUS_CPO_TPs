1b73d557839c710da97c5e046aa56c8584c3eb26
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-11-19 14:15:48 +0100

    ajout de print pour entrer les pseudos

--- SP4_console_LAJOUS _ROUBAUD/src/sp4_console_lajous/_roubaud/SP4_console_LAJOUS_ROUBAUD.java
+++ SP4_console_LAJOUS _ROUBAUD/src/sp4_console_lajous/_roubaud/SP4_console_LAJOUS_ROUBAUD.java
@@ -16,7 +16,9 @@ public class SP4_console_LAJOUS_ROUBAUD {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Joueur 1 entrez votre pseudo");
         String NJ1 = sc.nextLine();
+        System.out.println("Joueur 2 entrez votre pseudo");
         String NJ2 = sc.nextLine();
         Joueur J1 = new Joueur(NJ1);
         Joueur J2 = new Joueur(NJ2);
