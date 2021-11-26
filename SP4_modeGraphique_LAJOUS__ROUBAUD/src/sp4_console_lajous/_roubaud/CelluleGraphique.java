4cd0bcc386ca198e2b8253e77b32128f5a4db381
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-11-26 19:03:09 +0100

    interface graphique (jusq'à vidéo 4), joueurCourant aléatoire à rajouter

--- SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/CelluleGraphique.java
+++ SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/CelluleGraphique.java
@@ -15,7 +15,10 @@ import javax.swing.JButton;
 public class CelluleGraphique extends JButton{
     Cellule celluleAssociee;
     ImageIcon img_vide = new javax.swing.ImageIcon(getClass().getResource("/images/celluleVide.png"));
-    
+    ImageIcon img_desint = new javax.swing.ImageIcon(getClass().getResource("/images/desintegrateur.png"));
+    ImageIcon img_jaune = new javax.swing.ImageIcon(getClass().getResource("/images/jetonJaune.png"));
+    ImageIcon img_rouge = new javax.swing.ImageIcon(getClass().getResource("/images/jetonRouge.png"));
+    ImageIcon img_noir = new javax.swing.ImageIcon(getClass().getResource("/images/trouNoir.png"));
     
     
     public CelluleGraphique(Cellule UneCelulle){
@@ -25,6 +28,21 @@ public class CelluleGraphique extends JButton{
     @Override
     public void paintComponent(Graphics G){
         super.paintComponent(G);
-        setIcon(img_vide); // on attribue l'image celluleVide.png
+        String couleur_jeton = celluleAssociee.lireCouleurDuJeton();
+        
+        if(celluleAssociee.presenceTrouNoir()){
+            setIcon(img_noir);
+        }else if(celluleAssociee.presenceDesintegrateur()){
+            setIcon(img_desint);
+        }else{
+            switch(couleur_jeton){
+                case "rouge":
+                    setIcon(img_rouge);
+                case "jaune":
+                    setIcon(img_jaune);
+                case "nd":
+                    setIcon(img_vide); // on attribue l'image celluleVide.png
+            }
+        }
     }
 }
