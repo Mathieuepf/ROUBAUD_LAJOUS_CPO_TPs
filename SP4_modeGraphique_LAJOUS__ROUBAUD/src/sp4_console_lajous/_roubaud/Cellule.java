04b54a4a6f1a9c32615bfc82402b7e69cac57957
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-11-26 16:06:56 +0100

    création de la version console

---
+++ SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/Cellule.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sp4_console_lajous._roubaud;
+
+import java.util.Random;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class Cellule {
+    Jeton jetonCourant;
+    boolean trouNoir;
+    boolean desintegrateur;
+    
+    public Cellule(){
+        
+    }
+    public boolean affecterJeton(Jeton JetonEntree){
+        if(jetonCourant == null){
+            jetonCourant = JetonEntree;
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public String lireCouleurDuJeton(){
+        if(jetonCourant != null){
+            return jetonCourant.Couleur;
+        }else{
+            return("nd");
+        }
+    }
+    public boolean placerTrouNoir (){
+        if (trouNoir){
+            return false;
+        }
+        else{
+            trouNoir=true;   
+            return true;
+        }
+    }
+    public boolean presenceTrouNoir (){
+        return trouNoir;
+    }
+    public boolean activerTrouNoir (){
+        if (trouNoir){
+            jetonCourant=null;
+            trouNoir=false; 
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    
+    public Jeton recupererJeton(){
+        return jetonCourant;
+    }
+    public boolean supprimerJeton(){
+        if (jetonCourant==null){
+            return false;
+        }
+        else{
+            jetonCourant=null;
+            return true;
+        }
+    }
+    
+    
+    public boolean placerDesintegrateur (){
+        if (desintegrateur){
+            return false;
+        }
+        else{
+            desintegrateur=true;   
+            return true;
+        }
+    }
+    public boolean presenceDesintegrateur (){
+        if (desintegrateur){
+            return true;
+        }
+        else{   
+            return false;
+        }
+    }
+    public boolean recupererDesingrateur (){
+        if (desintegrateur){
+            desintegrateur=false;
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
---
+++ SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/Joueur.java
@@ -0,0 +1,53 @@
+/*
+    SP4 / Console : Classe Joueur
+    LAJOUS Malô & ROUBAUD Mathieu
+    Novembre 2021
+*/
+package sp4_console_lajous._roubaud;
+
+/**
+ *
+ * @author malol
+ */
+public class Joueur {
+    String nom;
+    String Couleur;
+    Jeton [] ListeJetons;
+    int nombreJetonsRestants;
+    int nombreDesintegrateurs;
+    
+    public Joueur (String player){
+        nom=player;
+    }
+    public void affecterCouleur (String c){
+     Couleur=c;   
+    }
+    public void ajouterJeton (Jeton n){
+        if(ListeJetons == null){
+            ListeJetons = new Jeton[1];
+            ListeJetons[0] = n;
+            nombreJetonsRestants = 1;
+        }else{
+            Jeton [] SaveList = ListeJetons;
+            ListeJetons = new Jeton[ListeJetons.length+1];
+            for(int i=0 ; i<SaveList.length ; i++){
+                ListeJetons[i] = SaveList[i];
+            }
+            ListeJetons[ListeJetons.length-1]=n;
+            nombreJetonsRestants += 1;
+        }
+        
+    }
+    public void obtenirDesintegrateur(){
+        nombreDesintegrateurs=nombreDesintegrateurs+1;
+    }
+    public boolean utiliserDesintegrateur(){
+        if (nombreDesintegrateurs<=0){
+            return false;
+        }
+        else{
+            nombreDesintegrateurs=nombreDesintegrateurs-1;
+            return true;
+        }
+    }
+}
