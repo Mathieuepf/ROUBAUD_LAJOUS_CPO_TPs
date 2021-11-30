41ffac8ba1ce2c2c6ef56a5f8251e5fbf9efeccd
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-11-30 20:37:15 +0100

    correction bug suite à récupération jeton

--- SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/Joueur.java
+++ SP4_modeGraphique_LAJOUS__ROUBAUD/src/sp4_console_lajous/_roubaud/Joueur.java
@@ -2,7 +2,7 @@
     SP4 / Console : Classe Joueur
     LAJOUS Malô & ROUBAUD Mathieu
     Novembre 2021
-*/
+ */
 package sp4_console_lajous._roubaud;
 
 /**
@@ -10,43 +10,53 @@ package sp4_console_lajous._roubaud;
  * @author malol
  */
 public class Joueur {
+
     String nom;
     String Couleur;
-    Jeton [] ListeJetons;
+    Jeton[] ListeJetons;
     int nombreJetonsRestants;
     int nombreDesintegrateurs;
-    
-    public Joueur (String player){
-        nom=player;
+
+    public Joueur(String player) {
+        nom = player;
     }
-    public void affecterCouleur (String c){
-     Couleur=c;   
+
+    public void affecterCouleur(String c) {
+        Couleur = c;
     }
-    public void ajouterJeton (Jeton n){
-        if(ListeJetons == null){
+
+    public void ajouterJeton(Jeton n) {
+        if (ListeJetons == null) {
             ListeJetons = new Jeton[1];
             ListeJetons[0] = n;
             nombreJetonsRestants = 1;
-        }else{
-            Jeton [] SaveList = ListeJetons;
-            ListeJetons = new Jeton[ListeJetons.length+1];
-            for(int i=0 ; i<SaveList.length ; i++){
+        } else {
+            Jeton[] SaveList = ListeJetons;
+            ListeJetons = new Jeton[nombreJetonsRestants + 1];
+            for (int i = 0; i < nombreJetonsRestants; i++) {
                 ListeJetons[i] = SaveList[i];
             }
-            ListeJetons[ListeJetons.length-1]=n;
+            ListeJetons[nombreJetonsRestants] = n;
             nombreJetonsRestants += 1;
+            for (int i = 0; i < ListeJetons.length; i++) {
+                if (ListeJetons[i] == null) {
+                    System.out.print(i + "NULL ");
+                } else {
+                    System.out.print(i + "P ");
+                }
+            }
         }
-        
     }
-    public void obtenirDesintegrateur(){
-        nombreDesintegrateurs=nombreDesintegrateurs+1;
+
+    public void obtenirDesintegrateur() {
+        nombreDesintegrateurs = nombreDesintegrateurs + 1;
     }
-    public boolean utiliserDesintegrateur(){
-        if (nombreDesintegrateurs<=0){
+
+    public boolean utiliserDesintegrateur() {
+        if (nombreDesintegrateurs <= 0) {
             return false;
-        }
-        else{
-            nombreDesintegrateurs=nombreDesintegrateurs-1;
+        } else {
+            nombreDesintegrateurs = nombreDesintegrateurs - 1;
             return true;
         }
     }
