e21b53ed9b5f665c8e36015157dc92fb6c749602
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2021-12-10 12:14:24 +0100

    création des classes jeton et jetonReponse

---
+++ Mastermind_Roubaud/src/mastermind_roubaud/Jeton.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_roubaud;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class Jeton {
+    String couleur;
+    
+    public Jeton(String c){
+        couleur = c;
+    }
+}
---
+++ Mastermind_Roubaud/src/mastermind_roubaud/JetonReponse.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mastermind_roubaud;
+
+/**
+ *
+ * @author ROUBAUD Mathieu
+ */
+public class JetonReponse {
+    String couleur; //rouge ou blanc
+    boolean placee; //jeton placee ou non
+    
+    public JetonReponse(boolean p){
+        placee = p;
+        if(p){
+            couleur = "rouge";
+        }else{
+            couleur = "blanc";
+        }
+    }
+}
