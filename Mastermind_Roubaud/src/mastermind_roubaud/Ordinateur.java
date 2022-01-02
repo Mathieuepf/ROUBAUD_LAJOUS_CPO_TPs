02bb802c2449fa7e8b30a46388f8ed2a3a6159e0
ROUBAUD Mathieu <ROUBAUD Mathieu@LAPTOP-C72M6G23>
2022-01-03 00:00:10 +0100

    fin ?

--- Mastermind_Roubaud/src/mastermind_roubaud/Ordinateur.java
+++ Mastermind_Roubaud/src/mastermind_roubaud/Ordinateur.java
@@ -16,7 +16,7 @@ public class Ordinateur {
     public void creerUneCombinaison(){
         for(int i=0 ; i<4 ; i++){
             int Rand = new Random().nextInt(5);
-            System.out.println(Rand);
+            
             Wcombine[i] = new Jeton(Rand);
         }
     }
